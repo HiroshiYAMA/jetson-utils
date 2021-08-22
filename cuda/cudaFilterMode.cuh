0ff2a94d8ff5efbbaf3e81d6a56c3eadfb4a2653
Hiroshi Yamamoto <hiyamamoto@mac.com>
2021-08-22 15:46:09 +0900

    Improve resize function with converting output data type and adding filter type.

--- cuda/cudaFilterMode.cuh
+++ cuda/cudaFilterMode.cuh
@@ -25,6 +25,7 @@
 
 
 #include "cudaFilterMode.h"
+#include "cudaVector.h"
 #include "cudaMath.h"
 
 
@@ -80,6 +81,409 @@ float4 cudaReadPixel<FORMAT_CHW>( float4* input, int x, int y, int width, int he
 
 ///@}
 
+
+
+// linear.
+template<cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel_linear( T* input, float x, float y, int width, int height, float max_value=255.0f )
+{
+	const int x1 = __float2int_rd(x);
+	const int y1 = __float2int_rd(y);
+	const int x2 = x1 + 1;
+	const int y2 = y1 + 1;
+	const int x2_read = ::min(x2, width - 1);
+	const int y2_read = ::min(y2, height - 1);
+
+	const float4 pix_in[4] = {
+		make_float4(cudaReadPixel<format>(input, x1, y1, width, height)),
+		make_float4(cudaReadPixel<format>(input, x2_read, y1, width, height)),
+		make_float4(cudaReadPixel<format>(input, x1, y2_read, width, height)),
+		make_float4(cudaReadPixel<format>(input, x2_read, y2_read, width, height)),
+	};
+	const float weight[4] = {
+		(x2 - x) * (y2 - y),
+		(x - x1) * (y2 - y),
+		(x2 - x) * (y - y1),
+		(x - x1) * (y - y1),
+	};
+
+	const float4 pix_tmp =
+		pix_in[0] * weight[0]
+		+ pix_in[1] * weight[1]
+		+ pix_in[2] * weight[2]
+		+ pix_in[3] * weight[3];
+
+	const T out = cast_vec<T>(clamp(pix_tmp, 0.0f, max_value));
+
+	return out;
+}
+
+
+
+// cubic.
+template <typename T>
+static __device__ inline T calc_cubic_coef(T d, T a)
+{
+	T d_abs = abs(d);
+
+	// T w1 = T(1) - (a + T(3)) * d_abs * d_abs + (a + T(2)) * d_abs * d_abs * d_abs;
+	// T w2 = T(-4) * a + T(8) * a * d_abs + T(-5) * a * d_abs * d_abs + a * d_abs * d_abs * d_abs;
+	T w1 = T(1) + ((-a + T(-3)) + (a + T(2)) * d_abs) * d_abs * d_abs;
+	T w2 = (T(-4) + (T(8) + (T(-5) + d_abs) * d_abs) * d_abs) * a;
+
+	T w = (d_abs < T(1)) ? w1 : (d_abs < T(2)) ? w2 : T(0);
+
+	return w;
+}
+static __device__ inline float calc_cubic_coef_f(float d, float a)
+{
+	float d_abs = abs(d);
+
+	float w1 = __fmaf_rn(__fmaf_rn(a + 2.0f, d_abs, -a - 3.0f), d_abs * d_abs, 1.0f);
+	float w2 = __fmaf_rn(__fmaf_rn(__fmaf_rn(d_abs, 1.0f, -5.0f), d_abs, 8.0f), d_abs, -4.0f) * a;
+
+	float w = (d_abs < 1.0f) ? w1 : (d_abs < 2.0f) ? w2 : 0.0f;
+
+	return w;
+}
+template<cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel_cubic( T* input, float x, float y, int width, int height, float max_value = 255.0f )
+{
+	const int xc = __float2int_rd(x);
+	const int yc = __float2int_rd(y);
+	const int x_btm = xc - 1;
+	const int y_btm = yc - 1;
+	// const int x_top = xc + 2;
+	const int y_top = yc + 2;
+	const float xd = x - xc;
+	const float yd = y - yc;
+
+	constexpr float a = -0.75f;	// CPU ver. = -0.75, GPU ver. = -0.5
+	const float wx[4] = {
+		calc_cubic_coef_f(-1.0f - xd, a),
+		calc_cubic_coef_f(0.0f - xd, a),
+		calc_cubic_coef_f(1.0f - xd, a),
+		calc_cubic_coef_f(2.0f - xd, a),
+	};
+	const float wx_sum = wx[0] + wx[1] + wx[2] + wx[3];
+
+	float4 pix_sum = {};
+	float w_sum = 0.0f;
+	for (int i = y_btm; i <= y_top; i++) {
+		const float wy = calc_cubic_coef_f(i - yd - yc, a);
+
+		const int pos_x[4] = {
+			::max(x_btm, 0),
+			x_btm + 1,
+			::min(x_btm + 2, width - 1),
+			::min(x_btm + 3, width - 1),
+		};
+		const int pos_y = ::clamp(i, 0, height - 1);
+
+		const float4 pix[4] = {
+			make_float4(cudaReadPixel<format>(input, pos_x[0], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[1], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[2], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[3], pos_y, width, height)),
+		};
+
+		pix_sum += (pix[0] * wx[0] + pix[1] * wx[1] + pix[2] * wx[2] + pix[3] * wx[3]) * wy;
+		w_sum += wx_sum * wy;
+	}
+
+	// const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp((pix_sum / w_sum), 0.0f, max_value));
+	float4 pix_out = {
+		__fdividef(pix_sum.x, w_sum),
+		__fdividef(pix_sum.y, w_sum),
+		__fdividef(pix_sum.z, w_sum),
+		__fdividef(pix_sum.w, w_sum),
+	};
+	const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp(pix_out, 0.0f, max_value));
+
+	return out;
+}
+
+
+
+// area.
+template<cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel_area( T* input, float x, float y, int width, int height, float2 scale, float max_value = 255.0f )
+{
+	float fsx1 = x;
+	float fsx2 = ::min(fsx1 + scale.x, (float)width);
+
+	int sx1 = __float2int_ru(fsx1);
+	int sx2 = __float2int_rd(fsx2);
+
+	float fsy1 = y;
+	float fsy2 = ::min(fsy1 + scale.y, (float)height);
+
+	int sy1 = __float2int_ru(fsy1);
+	int sy2 = __float2int_rd(fsy2);
+
+	float sx = ::min(float(scale.x), width - fsx1);
+	float sy = ::min(float(scale.y), height - fsy1);
+	float scale_xy = 1.f / (sx * sy);
+
+	float4 out = {};
+
+	for (int dy = sy1; dy < sy2; ++dy)
+	{
+		// inner rectangle.
+		for (int dx = sx1; dx < sx2; ++dx)
+			out += make_float4(cudaReadPixel<format>(input, dx, dy, width, height));
+
+		// v-edge line(left).
+		if (sx1 > fsx1)
+			out += make_float4(cudaReadPixel<format>(input, (sx1 -1), dy, width, height) * (sx1 - fsx1));
+
+		// v-edge line(right).
+		if (sx2 < fsx2)
+			out += make_float4(cudaReadPixel<format>(input, sx2, dy, width, height) * (fsx2 - sx2));
+	}
+
+	// h-edge line(top).
+	if (sy1 > fsy1)
+		for (int dx = sx1; dx < sx2; ++dx)
+			out += make_float4(cudaReadPixel<format>(input, dx, (sy1 - 1), width, height) * (sy1 - fsy1));
+
+	// h-edge line(bottom).
+	if (sy2 < fsy2)
+		for (int dx = sx1; dx < sx2; ++dx)
+			out += make_float4(cudaReadPixel<format>(input, dx, sy2, width, height) * (fsy2 - sy2));
+
+	// corner(top, left).
+	if ((sy1 > fsy1) &&  (sx1 > fsx1))
+		out += make_float4(cudaReadPixel<format>(input, (sx1 - 1), (sy1 - 1), width, height) * (sy1 - fsy1) * (sx1 - fsx1));
+
+	// corner(top, right).
+	if ((sy1 > fsy1) &&  (sx2 < fsx2))
+		out += make_float4(cudaReadPixel<format>(input, sx2, (sy1 - 1), width, height) * (sy1 - fsy1) * (fsx2 - sx2));
+
+	// corner(bottom, left).
+	if ((sy2 < fsy2) &&  (sx2 < fsx2))
+		out += make_float4(cudaReadPixel<format>(input, sx2, sy2, width, height) * (fsy2 - sy2) * (fsx2 - sx2));
+
+	// corner(bottom, right).
+	if ((sy2 < fsy2) &&  (sx1 > fsx1))
+		out += make_float4(cudaReadPixel<format>(input, (sx1 - 1), sy2, width, height) * (fsy2 - sy2) * (sx1 - fsx1));
+
+	out *= scale_xy;
+
+	return cast_vec<T>(clamp(out, 0.0f, max_value));
+}
+
+
+
+// lanczos4.
+//// /* # SLOW. */
+// template <typename T>
+// static __device__  inline T calc_sinc(T x)
+// {
+// 	return (x == T(0)) ? T(1) : sin(x * M_PI) / (x * M_PI);
+// }
+// template <typename T>
+// static __device__ inline T calc_lanczos_coef(T d, T n)
+// {
+// 	T d_abs = abs(d);
+// 	T w = (d_abs > n) ? T(0) : calc_sinc(d_abs) * calc_sinc(d_abs / n);
+
+// 	return w;
+// }
+//// /* # FAST. */
+template <typename T>
+static __device__ inline T calc_lanczos_coef(T d, T n)
+{
+	T d_abs = abs(d);
+
+	T pi_d = M_PI * d_abs;
+	T cos_k1 = cos(pi_d * 0.25f);
+	T cos_k2 = sqrt(1.0f - cos_k1 * cos_k1);
+
+	constexpr auto zero = 1e-3f;
+	T w = (d_abs >= n) ? T(0) :
+		(d_abs < T(zero)) ? T(1) :
+		(T(16) * cos_k1 * cos_k2 * (cos_k1 - cos_k2) * (cos_k1 + cos_k2) * cos_k2) / (pi_d * pi_d);
+
+	return w;
+}
+static __device__ inline float calc_lanczos_coef_f(float d, float n)
+{
+	float d_abs = abs(d);
+
+	float pi_d = (float)M_PI * d_abs;
+	float cos_k1 = __cosf(pi_d * 0.25f);
+	float cos_k2 = __fsqrt_rn(1.0f - cos_k1 * cos_k1);
+
+	constexpr auto zero = 1e-3f;
+	float w = (d_abs >= n) ? 0.0f :
+		(d_abs < zero) ? 1.0f :
+		__fdividef(16.0f * cos_k1 * cos_k2 * (cos_k1 - cos_k2) * (cos_k1 + cos_k2) * cos_k2, (pi_d * pi_d));
+
+	return w;
+}
+template<cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel_lanczos4( T* input, float x, float y, int width, int height, float max_value = 255.0f )
+{
+	constexpr float tap = 4.0f;
+
+	const int xc = __float2int_rd(x);
+	const int yc = __float2int_rd(y);
+	const int x_btm = xc - 3;
+	const int y_btm = yc - 3;
+	// const int x_top = xc + 4;
+	const int y_top = yc + 4;
+	const float xd = x - xc;
+	const float yd = y - yc;
+
+	const float wx[8] = {
+		calc_lanczos_coef_f(-3.0f - xd, tap),
+		calc_lanczos_coef_f(-2.0f - xd, tap),
+		calc_lanczos_coef_f(-1.0f - xd, tap),
+		calc_lanczos_coef_f(0.0f - xd, tap),
+		calc_lanczos_coef_f(1.0f - xd, tap),
+		calc_lanczos_coef_f(2.0f - xd, tap),
+		calc_lanczos_coef_f(3.0f - xd, tap),
+		calc_lanczos_coef_f(4.0f - xd, tap),
+	};
+	const float wx_sum = wx[0] + wx[1] + wx[2] + wx[3] + wx[4] + wx[5] + wx[6] + wx[7];
+
+	float4 pix_sum = {};
+	float w_sum = 0.0f;
+	for (int i = y_btm; i <= y_top; i++) {
+		const float wy = calc_lanczos_coef_f(i - yd - yc, tap);
+
+		const int pos_x[8] = {
+			::max(x_btm, 0),
+			::max(x_btm + 1, 0),
+			::max(x_btm + 2, 0),
+			x_btm + 3,
+			::min(x_btm + 4, width - 1),
+			::min(x_btm + 5, width - 1),
+			::min(x_btm + 6, width - 1),
+			::min(x_btm + 7, width - 1),
+		};
+		const int pos_y = ::clamp(i, 0, height - 1);
+
+		const float4 pix[8] = {
+			make_float4(cudaReadPixel<format>(input, pos_x[0], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[1], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[2], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[3], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[4], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[5], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[6], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[7], pos_y, width, height)),
+		};
+
+		const float4 pix_0 = pix[0] * wx[0] + pix[1] * wx[1] + pix[2] * wx[2] + pix[3] * wx[3];
+		const float4 pix_1 = pix[4] * wx[4] + pix[5] * wx[5] + pix[6] * wx[6] + pix[7] * wx[7];
+		pix_sum += (pix_0 + pix_1) * wy;
+		w_sum += wx_sum * wy;
+	}
+
+	// const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp(pix_sum / w_sum, 0.0f, max_value));
+	float4 pix_out = {
+		__fdividef(pix_sum.x, w_sum),
+		__fdividef(pix_sum.y, w_sum),
+		__fdividef(pix_sum.z, w_sum),
+		__fdividef(pix_sum.w, w_sum),
+	};
+	const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp(pix_out, 0.0f, max_value));
+
+	return out;
+}
+
+
+
+// spline36.
+template <typename T>
+static __device__ inline T calc_spline36_coef(T d)
+{
+	T d_abs = abs(d);
+
+	T w = (d_abs > T(3)) ? T(0)
+		: (d_abs > T(2)) ? (((T(19) * d_abs + T(-159)) * d_abs + T(434)) * d_abs + T(-384)) / T(209)
+		: (d_abs > T(1)) ? (((T(-114) * d_abs + T(612)) * d_abs + T(-1038)) * d_abs + T(540)) / T(209)
+		: (((T(247) * d_abs + T(-453)) * d_abs + T(-3)) * d_abs + T(209)) / T(209);
+
+	return w;
+}
+static __device__ inline float calc_spline36_coef_f(float d)
+{
+	float d_abs = abs(d);
+
+	float w = (d_abs > 3.0f) ? 0.0f
+		: (d_abs > 2.0f) ? __fdividef(__fmaf_rn(__fmaf_rn(__fmaf_rn(19.0f, d_abs, -159.0f), d_abs, 434.0f), d_abs, -384.0f), 209.0f)
+		: (d_abs > 1.0f) ? __fdividef(__fmaf_rn(__fmaf_rn(__fmaf_rn(-114.0f, d_abs, 612.0f), d_abs, -1038.0f), d_abs, 540.0f), 209.0f)
+		: __fdividef(__fmaf_rn(__fmaf_rn(__fmaf_rn(247.0f, d_abs, -453.0f), d_abs, -3.0f), d_abs, 209.0f), 209.0f);
+
+	return w;
+}
+template<cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel_spline36( T* input, float x, float y, int width, int height, float max_value = 255.0f )
+{
+	const int xc = __float2int_rd(x);
+	const int yc = __float2int_rd(y);
+	const int x_btm = xc - 2;
+	const int y_btm = yc - 2;
+	// const int x_top = xc + 3;
+	const int y_top = yc + 3;
+	const float xd = x - xc;
+	const float yd = y - yc;
+
+	const float wx[6] = {
+		calc_spline36_coef_f(-2.0f - xd),
+		calc_spline36_coef_f(-1.0f - xd),
+		calc_spline36_coef_f(0.0f - xd),
+		calc_spline36_coef_f(1.0f - xd),
+		calc_spline36_coef_f(2.0f - xd),
+		calc_spline36_coef_f(3.0f - xd),
+	};
+	const float wx_sum = wx[0] + wx[1] + wx[2] + wx[3] + wx[4] + wx[5];
+
+	float4 pix_sum = {};
+	float w_sum = 0.0f;
+	for (int i = y_btm; i <= y_top; i++) {
+		const float wy = calc_spline36_coef_f(i - yd - yc);
+
+		const int pos_x[6] = {
+			::max(x_btm, 0),
+			::max(x_btm + 1, 0),
+			x_btm + 2,
+			::min(x_btm + 3, width - 1),
+			::min(x_btm + 4, width - 1),
+			::min(x_btm + 5, width - 1),
+		};
+		const int pos_y = ::clamp(i, 0, height - 1);
+
+		const float4 pix[6] = {
+			make_float4(cudaReadPixel<format>(input, pos_x[0], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[1], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[2], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[3], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[4], pos_y, width, height)),
+			make_float4(cudaReadPixel<format>(input, pos_x[5], pos_y, width, height)),
+		};
+
+		const float4 pix6 = pix[0] * wx[0] + pix[1] * wx[1] + pix[2] * wx[2]
+							+ pix[3] * wx[3] + pix[4] * wx[4] + pix[5] * wx[5];
+		pix_sum += pix6 * wy;
+		w_sum += wx_sum * wy;
+	}
+
+	// const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp(pix_sum / w_sum, 0.0f, max_value));
+	float4 pix_out = {
+		__fdividef(pix_sum.x, w_sum),
+		__fdividef(pix_sum.y, w_sum),
+		__fdividef(pix_sum.z, w_sum),
+		__fdividef(pix_sum.w, w_sum),
+	};
+	const T out = (w_sum == 0.0f) ? T{} : cast_vec<T>(clamp(pix_out, 0.0f, max_value));
+
+	return out;
+}
+
 /**
  * CUDA device function for sampling a pixel with bilinear or point filtering.
  * cudaFilterPixel() is for use inside of other CUDA kernels, and accepts a
@@ -96,7 +500,7 @@ float4 cudaReadPixel<FORMAT_CHW>( float4* input, int x, int y, int width, int he
  * @ingroup cudaFilter
  */ 
 template<cudaFilterMode filter, cudaDataFormat format=FORMAT_HWC, typename T>
-__device__ inline T cudaFilterPixel( T* input, float x, float y, int width, int height )
+__device__ inline T cudaFilterPixel( T* input, float x, float y, int width, int height, float max_value = 255.0f )
 {
 	if( filter == FILTER_POINT )
 	{
@@ -105,42 +509,21 @@ __device__ inline T cudaFilterPixel( T* input, float x, float y, int width, int 
 
 		return cudaReadPixel<format>(input, x1, y1, width, height); //input[y1 * width + x1];
 	}
+	else if ( filter == FILTER_CUBIC )
+	{
+		return cudaFilterPixel_cubic<format>(input, x, y, width, height, max_value);
+	}
+	else if ( filter == FILTER_LANCZOS4 )
+	{
+		return cudaFilterPixel_lanczos4<format>(input, x, y, width, height, max_value);
+	}
+	else if ( filter == FILTER_SPLINE36 )
+	{
+		return cudaFilterPixel_spline36<format>(input, x, y, width, height, max_value);
+	}
 	else // FILTER_LINEAR
 	{
-		const float bx = x - 0.5f;
-		const float by = y - 0.5f;
-
-		const float cx = bx < 0.0f ? 0.0f : bx;
-		const float cy = by < 0.0f ? 0.0f : by;
-
-		const int x1 = int(cx);
-		const int y1 = int(cy);
-			
-		const int x2 = x1 >= width - 1 ? x1 : x1 + 1;	// bounds check
-		const int y2 = y1 >= height - 1 ? y1 : y1 + 1;
-		
-		const T samples[4] = {
-			cudaReadPixel<format>(input, x1, y1, width, height),   //input[y1 * width + x1],
-			cudaReadPixel<format>(input, x2, y1, width, height),   //input[y1 * width + x2],
-			cudaReadPixel<format>(input, x1, y2, width, height),   //input[y2 * width + x1],
-			cudaReadPixel<format>(input, x2, y2, width, height) }; //input[y2 * width + x2] };
-
-		// compute bilinear weights
-		const float x1d = cx - float(x1);
-		const float y1d = cy - float(y1);
-
-		const float x1f = 1.0f - x1d;
-		const float y1f = 1.0f - y1d;
-
-		const float x2f = 1.0f - x1f;
-		const float y2f = 1.0f - y1f;
-
-		const float x1y1f = x1f * y1f;
-		const float x1y2f = x1f * y2f;
-		const float x2y1f = x2f * y1f;
-		const float x2y2f = x2f * y2f;
-
-		return samples[0] * x1y1f + samples[1] * x2y1f + samples[2] * x1y2f + samples[3] * x2y2f;
+		return cudaFilterPixel_linear<format>(input, x, y, width, height, max_value);
 	}
 }
 
@@ -160,18 +543,44 @@ __device__ inline T cudaFilterPixel( T* input, float x, float y, int width, int 
  * @returns the filtered pixel from the input image
  * @ingroup cudaFilter
  */ 
-template<cudaFilterMode filter, cudaDataFormat format=FORMAT_HWC, typename T>
-__device__ inline T cudaFilterPixel( T* input, int x, int y,
-						       int input_width, int input_height,
-						       int output_width, int output_height )
-{
-	const float px = float(x) / float(output_width) * float(input_width);
-	const float py = float(y) / float(output_height) * float(input_height);
+ template<cudaFilterMode filter, cudaDataFormat format=FORMAT_HWC, typename T>
+ __device__ inline T cudaFilterPixel( T* input, float x, float y,
+								int input_width, int input_height,
+								int output_width, int output_height,
+								float2 scale, float max_value = 255.0f )
+ {
+	const float px =
+	(filter == FILTER_POINT || filter == FILTER_AREA)
+		? (x * scale.x)
+		: ::max(((x + 0.5f) * scale.x - 0.5f), 0.0f);
+	const float py =
+	(filter == FILTER_POINT || filter == FILTER_AREA)
+		? (y * scale.y)
+		: ::max(((y + 0.5f) * scale.y - 0.5f), 0.0f);
 
-	return cudaFilterPixel<filter, format>(input, px, py, input_width, input_height);
+	if ( filter == FILTER_AREA ) {
+		if (scale.x > 1.0f && scale.y > 1.0f) {
+			return cudaFilterPixel_area<format>(input, px, py, input_width, input_height, scale, max_value);
+		} else {
+			return cudaFilterPixel_linear<format>(input, px, py, input_width, input_height, max_value);
+		}
+	} else {
+		return cudaFilterPixel<filter, format>(input, px, py, input_width, input_height, max_value);
+	}
+ }
+ template<cudaFilterMode filter, cudaDataFormat format=FORMAT_HWC, typename T>
+__device__ inline T cudaFilterPixel( T* input, float x, float y,
+						       int input_width, int input_height,
+						       int output_width, int output_height,
+							   float max_value = 255.0f )
+{
+	const float2 scale = {
+		__fdividef(float(input_width), float(output_width)),
+		__fdividef(float(input_height), float(output_height)),
+	};
+
+	return cudaFilterPixel<filter, format>(input, x, y, input_width, input_height, output_width, output_height, scale, max_value);
 }
 
 
 #endif
-
-
--- cuda/cudaMath.h
+++ cuda/cudaMath.h
@@ -81,6 +81,39 @@ inline float rsqrtf(float x)
 // constructors
 ////////////////////////////////////////////////////////////////////////////////
 
+inline __host__ __device__ uchar2 make_uchar2(uchar s)
+{
+    return make_uchar2(s, s);
+}
+inline __host__ __device__ uchar2 make_uchar2(float2 a)
+{
+    return make_uchar2(uchar(a.x), uchar(a.y));
+}
+inline __host__ __device__ uchar2 make_uchar2(float3 a)
+{
+    return make_uchar2(uchar(a.x), uchar(a.y));
+}
+inline __host__ __device__ uchar2 make_uchar2(float4 a)
+{
+    return make_uchar2(uchar(a.x), uchar(a.y));
+}
+inline __host__ __device__ uchar2 make_uchar2(int2 a)
+{
+    return make_uchar2(a.x, a.y);
+}
+inline __host__ __device__ uchar2 make_uchar2(uint2 a)
+{
+    return make_uchar2(a.x, a.y);
+}
+inline __host__ __device__ uchar2 make_uchar2(uchar3 a)
+{
+    return make_uchar2(a.x, a.y);
+}
+inline __host__ __device__ uchar2 make_uchar2(uchar4 a)
+{
+    return make_uchar2(a.x, a.y);
+}
+
 inline __host__ __device__ float2 make_float2(float s)
 {
     return make_float2(s, s);
@@ -89,11 +122,27 @@ inline __host__ __device__ float2 make_float2(float3 a)
 {
     return make_float2(a.x, a.y);
 }
+inline __host__ __device__ float2 make_float2(float4 a)
+{
+    return make_float2(a.x, a.y);
+}
 inline __host__ __device__ float2 make_float2(int2 a)
 {
     return make_float2(float(a.x), float(a.y));
 }
 inline __host__ __device__ float2 make_float2(uint2 a)
+{
+    return make_float2(float(a.x), float(a.y));
+}
+inline __host__ __device__ float2 make_float2(uchar2 a)
+{
+    return make_float2(float(a.x), float(a.y));
+}
+inline __host__ __device__ float2 make_float2(uchar3 a)
+{
+    return make_float2(float(a.x), float(a.y));
+}
+inline __host__ __device__ float2 make_float2(uchar4 a)
 {
     return make_float2(float(a.x), float(a.y));
 }
@@ -131,6 +180,10 @@ inline __host__ __device__ uint2 make_uint2(int2 a)
 inline __host__ __device__ float3 make_float3(uchar s)
 {
     return make_float3(s, s, s);
+}
+inline __host__ __device__ float3 make_float3(uchar2 a)
+{
+    return make_float3(float(a.x), float(a.y), 0.0f);
 }
 inline __host__ __device__ float3 make_float3(float s)
 {
@@ -215,6 +268,10 @@ inline __host__ __device__ uchar3 make_uchar3(uchar s)
 {
     return make_uchar3(s, s, s);
 }
+inline __host__ __device__ uchar3 make_uchar3(uchar2 a)
+{
+    return make_uchar3(a.x, a.y, 0);
+}
 inline __host__ __device__ uchar3 make_uchar3(uint s)
 {
     return make_uchar3(s, s, s);
@@ -243,6 +300,14 @@ inline __host__ __device__ uchar3 make_uchar3(int3 a)
 {
     return make_uchar3(uchar(a.x), uchar(a.y), uchar(a.z));
 }
+inline __host__ __device__ uchar3 make_uchar3(float s)
+{
+    return make_uchar3(s, s, s);
+}
+inline __host__ __device__ uchar3 make_uchar3(float2 a)
+{
+    return make_uchar3(a.x, a.y, 0);
+}
 inline __host__ __device__ uchar3 make_uchar3(float3 a)
 {
     return make_uchar3(a.x, a.y, a.z);
@@ -256,9 +321,21 @@ inline __host__ __device__ float4 make_float4(uchar s)
 {
     return make_float4(s, s, s, s);
 }
+inline __host__ __device__ float4 make_float4(uchar2 a)
+{
+    return make_float4(float(a.x), float(a.y), 0.0f, 0.0f);
+}
 inline __host__ __device__ float4 make_float4(float s)
 {
     return make_float4(s, s, s, s);
+}
+inline __host__ __device__ float4 make_float4(float2 a)
+{
+    return make_float4(a.x, a.y, 0.0f, 0.0f);
+}
+inline __host__ __device__ float4 make_float4(float2 a, float s)
+{
+    return make_float4(a.x, a.y, s, s);
 }
 inline __host__ __device__ float4 make_float4(float3 a)
 {
@@ -332,6 +409,10 @@ inline __host__ __device__ uchar4 make_uchar4(uchar s)
 {
     return make_uchar4(s, s, s, s);
 }
+inline __host__ __device__ uchar4 make_uchar4(uchar2 a)
+{
+    return make_uchar4(a.x, a.y, 0, 0);
+}
 inline __host__ __device__ uchar4 make_uchar4(uint s)
 {
     return make_uchar4(s, s, s, s);
@@ -355,6 +436,14 @@ inline __host__ __device__ uchar4 make_uchar4(uint3 a, uint w)
 inline __host__ __device__ uchar4 make_uchar4(int4 a)
 {
     return make_uchar4(uchar(a.x), uchar(a.y), uchar(a.z), uchar(a.w));
+}
+inline __host__ __device__ uchar4 make_uchar4(float s)
+{
+    return make_uchar4(s, s, s, s);
+}
+inline __host__ __device__ uchar4 make_uchar4(float2 a)
+{
+    return make_uchar4(a.x, a.y, 0, 0);
 }
 inline __host__ __device__ uchar4 make_uchar4(float3 a)
 {
@@ -1049,13 +1138,38 @@ inline __host__ __device__ void operator*=(uint3 &a, uint b)
     a.y *= b;
     a.z *= b;
 }
-inline __host__ __device__ uchar3 operator*(uchar3 a, float b)
+
+inline __host__ __device__ uchar3 operator*(uchar3 a, uchar3 b)
+{
+    return make_uchar3(a.x * b.x, a.y * b.y, a.z * b.z);
+}
+inline __host__ __device__ void operator*=(uchar3 &a, uchar3 b)
+{
+    a.x *= b.x;
+    a.y *= b.y;
+    a.z *= b.z;
+}
+inline __host__ __device__ uchar3 operator*(uchar3 a, uchar b)
 {
     return make_uchar3(a.x * b, a.y * b, a.z * b);
 }
-inline __host__ __device__ uchar3 operator*(float b, uchar3 a)
+inline __host__ __device__ uchar3 operator*(uchar b, uchar3 a)
 {
     return make_uchar3(b * a.x, b * a.y, b * a.z);
+}
+inline __host__ __device__ void operator*=(uchar3 &a, uchar b)
+{
+    a.x *= b;
+    a.y *= b;
+    a.z *= b;
+}
+inline __host__ __device__ float3 operator*(uchar3 a, float b)
+{
+    return make_float3(a.x * b, a.y * b, a.z * b);
+}
+inline __host__ __device__ float3 operator*(float b, uchar3 a)
+{
+    return make_float3(b * a.x, b * a.y, b * a.z);
 }
 inline __host__ __device__ void operator*=(uchar3 &a, float b)
 {
@@ -1171,6 +1285,21 @@ inline __host__ __device__ void operator*=(uchar4 &a, uchar b)
     a.z *= b;
     a.w *= b;
 }
+inline __host__ __device__ float4 operator*(uchar4 a, float b)
+{
+    return make_float4(a.x * b, a.y * b, a.z * b,  a.w * b);
+}
+inline __host__ __device__ float4 operator*(float b, uchar4 a)
+{
+    return make_float4(b * a.x, b * a.y, b * a.z, b * a.w);
+}
+inline __host__ __device__ void operator*=(uchar4 &a, float b)
+{
+    a.x *= b;
+    a.y *= b;
+    a.z *= b;
+    a.w *= b;
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // divide
@@ -1224,6 +1353,35 @@ inline __host__ __device__ float3 operator/(float b, float3 a)
     return make_float3(b / a.x, b / a.y, b / a.z);
 }
 
+inline __host__ __device__ uchar3 operator/(uchar3 a, uchar b)
+{
+    return make_uchar3(a.x / b, a.y / b, a.z / b);
+}
+inline __host__ __device__ uchar3 operator/(uchar b, uchar3 a)
+{
+    return make_uchar3(b / a.x, b / a.y, b / a.z);
+}
+inline __host__ __device__ void operator/=(uchar3 &a, uchar b)
+{
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+}
+inline __host__ __device__ float3 operator/(uchar3 a, float b)
+{
+    return make_float3(a.x / b, a.y / b, a.z / b);
+}
+inline __host__ __device__ float3 operator/(float b, uchar3 a)
+{
+    return make_float3(b / a.x, b / a.y, b / a.z);
+}
+inline __host__ __device__ void operator/=(uchar3 &a, float b)
+{
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+}
+
 inline __host__ __device__ float4 operator/(float4 a, float4 b)
 {
     return make_float4(a.x / b.x, a.y / b.y, a.z / b.z,  a.w / b.w);
@@ -1249,6 +1407,37 @@ inline __host__ __device__ void operator/=(float4 &a, float b)
 inline __host__ __device__ float4 operator/(float b, float4 a)
 {
     return make_float4(b / a.x, b / a.y, b / a.z, b / a.w);
+}
+
+inline __host__ __device__ uchar4 operator/(uchar4 a, uchar b)
+{
+    return make_uchar4(a.x / b, a.y / b, a.z / b, a.w / b);
+}
+inline __host__ __device__ uchar4 operator/(uchar b, uchar4 a)
+{
+    return make_uchar4(b / a.x, b / a.y, b / a.z, b / a.w);
+}
+inline __host__ __device__ void operator/=(uchar4 &a, uchar b)
+{
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+    a.w /= b;
+}
+inline __host__ __device__ float4 operator/(uchar4 a, float b)
+{
+    return make_float4(a.x / b, a.y / b, a.z / b, a.w / b);
+}
+inline __host__ __device__ float4 operator/(float b, uchar4 a)
+{
+    return make_float4(b / a.x, b / a.y, b / a.z, b / a.w);
+}
+inline __host__ __device__ void operator/=(uchar4 &a, float b)
+{
+    a.x /= b;
+    a.y /= b;
+    a.z /= b;
+    a.w /= b;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1376,6 +1565,10 @@ inline __device__ __host__ uint clamp(uint f, uint a, uint b)
 {
     return max(a, min(f, b));
 }
+inline __device__ __host__ uchar clamp(uchar f, uchar a, uchar b)
+{
+    return max(a, min(f, b));
+}
 
 inline __device__ __host__ float2 clamp(float2 v, float a, float b)
 {
@@ -1450,6 +1643,31 @@ inline __device__ __host__ uint4 clamp(uint4 v, uint a, uint b)
 inline __device__ __host__ uint4 clamp(uint4 v, uint4 a, uint4 b)
 {
     return make_uint4(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z), clamp(v.w, a.w, b.w));
+}
+
+inline __device__ __host__ uchar2 clamp(uchar2 v, uchar a, uchar b)
+{
+    return make_uchar2(clamp(v.x, a, b), clamp(v.y, a, b));
+}
+inline __device__ __host__ uchar2 clamp(uchar2 v, uchar2 a, uchar2 b)
+{
+    return make_uchar2(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y));
+}
+inline __device__ __host__ uchar3 clamp(uchar3 v, uchar a, uchar b)
+{
+    return make_uchar3(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b));
+}
+inline __device__ __host__ uchar3 clamp(uchar3 v, uchar3 a, uchar3 b)
+{
+    return make_uchar3(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z));
+}
+inline __device__ __host__ uchar4 clamp(uchar4 v, uchar a, uchar b)
+{
+    return make_uchar4(clamp(v.x, a, b), clamp(v.y, a, b), clamp(v.z, a, b), clamp(v.w, a, b));
+}
+inline __device__ __host__ uchar4 clamp(uchar4 v, uchar4 a, uchar4 b)
+{
+    return make_uchar4(clamp(v.x, a.x, b.x), clamp(v.y, a.y, b.y), clamp(v.z, a.z, b.z), clamp(v.w, a.w, b.w));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
