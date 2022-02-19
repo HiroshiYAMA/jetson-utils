134b7a48a816042b3343c283df16a75471a7d7fd
Hiroshi Yamamoto <hiyamamoto@mac.com>
2022-02-20 03:07:02 +0900

    Select NDI Send data format by #define NDI_SEND_PA16. RGBA or PA16.

--- NDI/NDI_Send.cpp
+++ NDI/NDI_Send.cpp
@@ -84,10 +84,14 @@ ndiSend* ndiSend::Create( const videoOptions& options )
 	frm.xres = opt.width;
 	frm.yres = opt.height;
 	// frm.FourCC = NDIlib_FourCC_type_BGRA;
-	// frm.FourCC = NDIlib_FourCC_type_RGBA;
 	// frm.FourCC = NDIlib_FourCC_type_UYVA;
+#ifdef NDI_SEND_PA16
 	frm.FourCC = NDIlib_FourCC_type_PA16;
 	frm.line_stride_in_bytes = opt.width * 1 * sizeof(uint16_t);
+#else
+	frm.FourCC = NDIlib_FourCC_type_RGBA;
+	frm.line_stride_in_bytes = opt.width * 4;
+#endif
 	frm.frame_rate_N = opt.frameRateNum;
 	frm.frame_rate_D = opt.frameRateDenom;
 	frm.frame_format_type = NDIlib_frame_format_type_progressive;
